30a2e03d889ba0a30992fc0ce0c076e0719e6ceb
wiikang84 <tmzkt2@gmail.com>
2026-06-18 12:47:01 +0900

    Bump wiiInfo asset version to 20260618-03

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (async function () {
-  const ASSET_VERSION = "20260618-02";
+  const ASSET_VERSION = "20260618-03";
   const LANGUAGES = window.WIIINFO_LANGUAGES || {};
   const LANGUAGE_NAMES = window.WIIINFO_LANGUAGE_NAMES || {};
   const PROFILES = window.WIIINFO_LEARNER_PROFILES || [];
